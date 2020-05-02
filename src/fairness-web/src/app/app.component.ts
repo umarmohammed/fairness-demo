62c57da699c07e70b30e3395cd3641d1ca826400
Umar Mohammed <umar@thinkincode.co.uk>
2020-05-02 18:42:35 +0100

    add netlify badge

--- src/fairness-web/src/app/app.component.ts
+++ src/fairness-web/src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `<router-outlet></router-outlet>
+    <a
+      style="position: absolute; right: 0; top: 0; z-index: 1;"
+      href="https://www.netlify.com"
+    >
+      <img
+        src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"
+        alt="Deploys by Netlify"
+      />
+    </a> `,
 })
 export class AppComponent {}
