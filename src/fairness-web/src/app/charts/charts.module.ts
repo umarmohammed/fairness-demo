bb5576e669a7b0583df81ec8baa819c0a3506bba
Umar Mohammed <umar@thinkincode.co.uk>
2020-05-05 16:36:26 +0100

    move combo chart into seperate folder

--- src/fairness-web/src/app/charts/charts.module.ts
+++ src/fairness-web/src/app/charts/charts.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { ComboChartComponent } from './combo-chart.component';
-import { ComboSeriesVerticalComponent } from './combo-series-vertical.component';
-import { ComboBarLabelComponent } from './combo-bar-label.component';
+import { ComboChartComponent } from './combo-chart/combo-chart.component';
+import { ComboSeriesVerticalComponent } from './combo-chart/combo-series-vertical.component';
+import { ComboBarLabelComponent } from './combo-chart/combo-bar-label.component';
 
 @NgModule({
   imports: [NgxChartsModule],
