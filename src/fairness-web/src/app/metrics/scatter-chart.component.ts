d562a23c5e6c33d5431920fb42f25e76cf52993e
Umar Mohammed <umar@thinkincode.co.uk>
2020-05-06 11:59:39 +0100

    add demo  scatter plot code

--- src/fairness-web/src/app/metrics/scatter-chart.component.ts
+++ src/fairness-web/src/app/metrics/scatter-chart.component.ts
@@ -6,15 +6,21 @@ import { Metrics, PerformanceMetric } from './metrics';
   template: `
     <p class="title">Scatter</p>
     <div class="performance-charts">
-      <ngx-charts-custom-bar-vertical
-        [results]="metrics.performance"
-        [yScaleMin]="0"
-        [yScaleMax]="1"
-        [yAxis]="true"
-        [showDataLabel]="true"
-        [xAxis]="true"
+      <ngx-charts-bubble-chart
+        [scheme]="colorScheme"
+        [results]="bubbleData"
+        [xAxis]="showXAxis"
+        [yAxis]="showYAxis"
+        [showXAxisLabel]="showXAxisLabel"
+        [showYAxisLabel]="showYAxisLabel"
+        [xAxisLabel]="xAxisLabel"
+        [yAxisLabel]="yAxisLabel"
+        [yScaleMin]="yScaleMin"
+        [yScaleMax]="yScaleMax"
+        [minRadius]="minRadius"
+        [maxRadius]="maxRadius"
       >
-      </ngx-charts-custom-bar-vertical>
+      </ngx-charts-bubble-chart>
     </div>
     <div class="select-grid">
       <div class="select-container">
@@ -78,4 +84,118 @@ import { Metrics, PerformanceMetric } from './metrics';
 })
 export class ScatterChartComponent {
   @Input() metrics: Metrics;
+
+  bubbleData = [
+    {
+      name: 'Germany',
+      series: [
+        {
+          name: '2010',
+          x: '2010',
+          y: 80.3,
+          r: 80.4,
+        },
+        {
+          name: '2000',
+          x: '2000',
+          y: 80.3,
+          r: 78,
+        },
+        {
+          name: '1990',
+          x: '1990',
+          y: 75.4,
+          r: 79,
+        },
+      ],
+    },
+    {
+      name: 'United States',
+      series: [
+        {
+          name: '2010',
+          x: '2010',
+          y: 78.8,
+          r: 310,
+        },
+        {
+          name: '2000',
+          x: '2000',
+          y: 76.9,
+          r: 283,
+        },
+        {
+          name: '1990',
+          x: '1990',
+          y: 75.4,
+          r: 253,
+        },
+      ],
+    },
+    {
+      name: 'France',
+      series: [
+        {
+          name: '2010',
+          x: '2010',
+          y: 81.4,
+          r: 63,
+        },
+        {
+          name: '2000',
+          x: '2000',
+          y: 79.1,
+          r: 59.4,
+        },
+        {
+          name: '1990',
+          x: '1990',
+          y: 77.2,
+          r: 56.9,
+        },
+      ],
+    },
+    {
+      name: 'United Kingdom',
+      series: [
+        {
+          name: '2010',
+          x: '2010',
+          y: 80.2,
+          r: 62.7,
+        },
+        {
+          name: '2000',
+          x: '2000',
+          y: 77.8,
+          r: 58.9,
+        },
+        {
+          name: '1990',
+          x: '1990',
+          y: 75.7,
+          r: 57.1,
+        },
+      ],
+    },
+  ];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = false;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Values';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Years';
+  maxRadius: number = 20;
+  minRadius: number = 5;
+  yScaleMin: number = 70;
+  yScaleMax: number = 85;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
 }
